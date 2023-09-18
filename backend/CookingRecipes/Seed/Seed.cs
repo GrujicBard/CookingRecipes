2db77fd5ec3a2ace8983e70f147f4e6b4688e5a9
GrujicBard <33715866+GrujicBard@users.noreply.github.com>
2023-09-18 11:31:12 +0200

    Changed database seeding.

--- backend/CookingRecipes/Seed/Seed.cs
+++ backend/CookingRecipes/Seed/Seed.cs
@@ -16,7 +16,8 @@ namespace CookingRecipes.Seed
     public class Seed
     {
         private readonly DataContext dataContext;
-        string csv_file_path = @"C:\Users\Bard\source\repos\CookingRecipes\backend\CookingRecipes\Assets\recipes.csv";
+        readonly string _csv_file_path = Path.Combine(AppContext.BaseDirectory, "Assets", "recipes.csv");
+        readonly int _numberOfRecipes = 50; // How many recipes to add to database
 
         public Seed(DataContext dataContext)
         {
@@ -25,15 +26,13 @@ namespace CookingRecipes.Seed
 
         public void SeedDataContext()
         {
-            var csvRecipes = ReadCsv(csv_file_path);
+            var csvRecipes = ReadCsv(_csv_file_path);
             Random rnd = new();
-            int counter = 0;
             List<DishType> recipeTypes = Enum.GetValues(typeof(DishType))
                 .Cast<DishType>()
                 .ToList();
-            List<CategoryTypes> categoryTypes = Enum.GetValues(typeof(CategoryTypes))
-                .Cast<CategoryTypes>()
-                .ToList();
+
+            int counter = 0;
 
             if (!dataContext.Recipes.Any())
             {
@@ -49,58 +48,13 @@ namespace CookingRecipes.Seed
                             ImagePath = recipe.ImageName,
                             Ingredients = recipe.Ingredients,
                             Difficulty = rnd.Next(4),
-                            RecipeType = recipeTypes[rnd.Next(recipeTypes.Count)],
-                            RecipeCategories = new List<RecipeCategory>()
-                            {
-                                new RecipeCategory() { Category = new Category(){ Name = categoryTypes[rnd.Next(categoryTypes.Count)].ToString() }
-                            },
-                        }
+                            RecipeType = recipeTypes[rnd.Next(recipeTypes.Count)]
                         });
-                    if (++counter > 12) break;
+                    if (++counter > _numberOfRecipes) break;
                 }
                 dataContext.Recipes.AddRange(recipes);
+                dataContext.SaveChanges();
             }
-
-            if (!dataContext.UserFavoriteRecipes.Any())
-            {
-
-                var recipe = csvRecipes.ElementAt(12);
-                var favoriteRecipes = new List<UserFavoriteRecipe>()
-                {
-                    new UserFavoriteRecipe()
-                    {
-                        Recipe = new Recipe()
-                        {
-                            Title= recipe.Title,
-                            Ingredients= recipe.Ingredients,
-                            Instructions= recipe.Instructions,
-                            ImagePath = recipe.ImageName,
-                            Difficulty = rnd.Next(4),
-                            RecipeType = recipeTypes[rnd.Next(recipeTypes.Count)],
-                            Reviews = new List<Review>()
-                            {
-                                new Review() {Comment="Very good recipe!", Rating = 5.0M,
-                                    User = new User(){UserName = "jack", Password = "1234", Email = "jack@gmail.com",
-                                        Role = new Role(){Name = "User"}}}
-                            }
-                        },
-                        User = new User()
-                        {
-                            UserName = "bard",
-                            Password = "1234",
-                            Email = "bard.grujic@gmail.com",
-                            Role = new Role()
-                            {
-                                Name = "Admin"
-                            }
-                        }
-
-                    }
-                };
-                dataContext.AddRange(favoriteRecipes);
-            }
-            dataContext.SaveChanges();
-
         }
 
         public IEnumerable<RecipeSeed> ReadCsv(string csv_file_path)
@@ -115,7 +69,6 @@ namespace CookingRecipes.Seed
                 using (var csvReader = new CsvReader(streamReader, csvConfig))
                 {
                     return csvReader.GetRecords<RecipeSeed>().ToList();
-                    //Debug.WriteLine(recipes.First().Instructions);
                 }
             }
         }
