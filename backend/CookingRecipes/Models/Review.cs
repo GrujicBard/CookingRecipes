9726ad050b46f6795ee8234601028af49888200f
GrujicBard <33715866+GrujicBard@users.noreply.github.com>
2023-09-19 09:32:14 +0200

    Updated models Review and Role.

--- backend/CookingRecipes/Models/Review.cs
+++ backend/CookingRecipes/Models/Review.cs
@@ -7,6 +7,7 @@ namespace CookingRecipes.Models
 {
     public class Review
     {
+        [Key]
         public int Id { get; set; }
         [ForeignKey("User")]
         public int UserId { get; set; }
