b80c0bf9a9f3a093ef84361b9c46d31c8fbf3937
GrujicBard <33715866+GrujicBard@users.noreply.github.com>
2023-10-16 18:06:49 +0200

    Added filter and refined recipe search

--- frontend/CookingRecipes.UI/src/app/components/features/user/recipe/recipes-list-user/recipes-list-user.component.ts
+++ frontend/CookingRecipes.UI/src/app/components/features/user/recipe/recipes-list-user/recipes-list-user.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import Category from 'src/app/models/category';
 import { CuisineType } from 'src/app/models/enums/cuisineType';
 import { DishType } from 'src/app/models/enums/dishType';
 import { RecipeType } from 'src/app/models/enums/recipeType';
 import Recipe from 'src/app/models/recipe';
+import RecipeCategory from 'src/app/models/recipeCategory';
 import { RecipeService } from 'src/app/services/recipe/recipe.service';
 
 @Component({
@@ -11,8 +13,23 @@ import { RecipeService } from 'src/app/services/recipe/recipe.service';
   styleUrls: ['./recipes-list-user.component.scss']
 })
 export class RecipesListUserComponent implements OnInit {
+  dishTypes = Object.keys(DishType).filter((item) => {
+    return isNaN(Number(item));
+  });
+  cuisineTypes = Object.keys(CuisineType).filter((item) => {
+    return isNaN(Number(item));
+  });
+  recipeTypes = Object.keys(RecipeType).filter((item) => {
+    return isNaN(Number(item));
+  });
+
+
   recipes!: Recipe[];
-  search_value: string = "";
+  searchValue: string = "";
+  recipeType: string = "";
+  dishType: string = "";
+  cuisineType: string = "";
+
 
   constructor(
     private _recipeService: RecipeService,
@@ -26,7 +43,9 @@ export class RecipesListUserComponent implements OnInit {
     this._recipeService.getRecipes()
       .subscribe({
         next: (recipes) => {
+          console.log(recipes);
           this.recipes = recipes;
+          this.filterRecipes();
         },
         error: (response) => {
           console.log(response);
@@ -34,22 +53,53 @@ export class RecipesListUserComponent implements OnInit {
       });
   }
 
-  getRecipesByTitle(title: string) {
-    if (title == "") {
+  onSelect() {
+    this.getRecipesByTitle();
+  }
+
+
+  getRecipesByTitle() {
+    if (this.searchValue == "") {
       this.getRecipes();
     }
     else {
-      this._recipeService.getRecipesByTitle(title)
+      this._recipeService.getRecipesByTitle(this.searchValue)
         .subscribe({
           next: (recipes) => {
             this.recipes = recipes;
+            this.filterRecipes();
           },
           error: (response) => {
             console.log(response);
           },
         });
     }
+
   }
+
+  filterRecipes() {
+
+    // Filter Category
+    if (this.recipeType) {
+      this.recipes = this.recipes.filter(({ recipeCategories }) =>
+        recipeCategories.find(recipeCategories => recipeCategories.category?.recipeType === this.recipeTypes.indexOf(this.recipeType)
+        ));
+    }
+
+    // Filter Dish Type
+    if (this.dishType) {
+      this.recipes = this.recipes.filter((recipe) =>
+        recipe.dishType === this.dishTypes.indexOf(this.dishType)
+      );
+    }
+    // Filter Cuisine
+    if (this.cuisineType) {
+      this.recipes = this.recipes.filter((recipe) =>
+        recipe.cuisineType === this.cuisineTypes.indexOf(this.cuisineType)
+      );
+    }
+  }
+
 
   public get DishType() {
     return DishType;
